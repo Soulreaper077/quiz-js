75b1a887611cbbacbe3a20f87e97332d437a2660
Jesse <jesseponce233@gmail.com>
2022-05-28 02:27:37 -0500

    setting up all variables and constants as well as getting the questions and answers to display correctly

--- assets/script.js
+++ assets/script.js
@@ -1,124 +1,149 @@
+
 var score = 0;
 var availableQuestions = [];
 var questionCounter = 0;
-var allQuestions = [
-    {
-        question: 'commonly used data types DO Not include:',
-        choices: 'strings',
-        choices: 'booleans',
-        choices: 'alerts',
-        choices: 'numbers',
-        answer: 1,
-    },
-    {
-        question: 'The condition in an if / else statement is enclosed with ___.',
-        choices: 'quotes',
-        choices: 'curly braces',
-        choices: 'parenthesis',
-        choices: 'square brackets',
-        answer: 2,
-    },
-    {
-        question: 'Arrays in JavaScript can be used to store ____.',
-        choices: 'numbers and strings',
-        choices: 'other arrays',
-        choices: 'booleans',
-        choices: 'all of the above',
-        answer: 3,
-    },
-    {
-        question: 'String values must be enclosed within ___ when being assigned to variables.',
-        choices: 'commas ',
-        choices: 'curly brackets',
-        choices: 'quotes',
-        choices: 'parenthesis',
-        answer: 4,
-    },
-    {
-        question: 'Avery useful tool used during development and debugging.for printing content to the debugger is: ',
-        choices: 'JavaScript',
-        choices: 'terminal/bash',
-        choices: 'for loops',
-        choices: 'console.log',
-        answer: '5',
-    }
-    
-]
 var currentQuestion = {};
 var time =90;
 var runningTimer;
 var username ="";
 var finalScore;
-var correctAnswer = true; 
-
-// Constants
-const MAX_QUESTIONS = 5;
+var correctAnswer = false; 
+var allQuestions = [
+    {
+        question: 'commonly used data types DO Not include:',
+        choice1: 'strings',
+        choice2: 'booleans',
+        choice3: 'alerts',
+        choice4: 'numbers',
+        answer: 3,
+    },
+    {
+        question: 'The condition in an if / else statement is enclosed with ___.',
+        choice1: 'quotes',
+        choice2: 'curly braces',
+        choice3: 'parenthesis',
+        choice4: 'square brackets',
+        answer: 2,
+    },
+    {
+        question: 'Arrays in JavaScript can be used to store ____.',
+        choice1: 'numbers and strings',
+        choice2: 'other arrays',
+        choice3: 'booleans',
+        choice4: 'all of the above',
+        answer: 4,
+    },
+    {
+        question: 'String values must be enclosed within ___ when being assigned to variables.',
+        choice1: 'commas ',
+        choice2: 'curly brackets',
+        choice3: 'quotes',
+        choice4: 'parenthesis',
+        answer: 3,
+    },
+    {
+        question: 'Avery useful tool used during development and debugging.for printing content to the debugger is: ',
+        choice1: 'JavaScript',
+        choice2: 'terminal/bash',
+        choice3: 'for loops',
+        choice4: 'console.log',
+        answer: '4',
+    }
+    
+]
+const questionLimit = 5;
+const timerEl = document.getElementById("timer");
 const startButton = document.getElementById("startButton");
 const container = document.getElementById("container");
 const question = document.getElementById("question");
-// const answerButtons = document.getElementById("answer-box");
+const ansButtons = Array.from(document.getElementsByClassName("btn"));
+console.log(ansButtons);
 const countDown = document.getElementById("timerBox");
 const highScores = document.getElementById("highScoresButton");
 const questionBox = document.getElementById("questionBox");
-
 //document.getElementById('btn1').innerText = allQuestions[0].choice1
-
 startButton.addEventListener("click", startGame);
 highScoresButton.addEventListener("click", highScores);
 
+function countdown() {
+    let timeLeft = 60;
+    timeLeft = setTimeout(function () {
+        timeLeft --;
+        if (timeLeft > 1) {
+            timer.textContent = timeLeft;
+            timeLeft;
+        }
+        else if (timeLeft ===1) {
+            timer.textContent = timeLeft;
+            timeLeft --;
+        }else {
+            timer.textContent = "";
+            
+            questionBox.classList.add("hide");
+        }
+
+    })
+}
 //function to start the game 
 function startGame(){
     // classList in order to change the css in this case getting the "hide" elements 
-    startButton.classList.add("hide");
-    highScores.classList.add("hide");
-    questionBox.classList.remove("hide");
-
+    startButton.classList.add("hide"); // hides the start btn 
+    highScores.classList.add("hide"); // hides high score btn 
+    questionBox.classList.remove("hide"); //remove to display the questions box and answers 
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...allQuestions];
+    availableQuestions = [...allQuestions]; // spread operator to display questions from array 
     console.log(availableQuestions);
-    newQuestion();   
+    //countdown();
+    newQuestion();   // return new question 
+    
 };
-
+// generate a new question for every turn 
 function newQuestion(){
     questionCounter++;
     const choiceIndex = Math.floor(Math.random() * availableQuestions.length);
-    console.log(choiceIndex);
+    //pulling the random question from the questions array 
     currentQuestion = availableQuestions[choiceIndex];
-    question.innerText = currentQuestion.question; 
-    console.log(question.innerText);
-
-    allQuestions.forEach(function(option) {
-        const answers = option.choices;
-       // const answers = document.createElement('button');
-        //answers.innerText = availableQuestions;
-        console.log(answers);
-        // query selector to call the class from the html 
-        const answerLi = document.querySelector(".btn-grid");
-    // i is representing or calling the function 
-    for(let i = 0; i < 5; i++){
-            console.log(i);
-
-    }
-    
-        
-        
-
-
-
-
-
-        
-
-        
-        
-        
-    
+    console.log(currentQuestion);
+    // change the text in the question box to display array question 
+    question.innerText = currentQuestion.question;   
+// getting the answer buttons a,b,c,d to display the correct questions 
+    ansButtons.forEach(function(choice) {
+        //getting the number dataset from the answer btn 
+        const number = choice.dataset["number"];
+        console.log(number);
+        //change the text to display answers for each btn 
+        choice.innerText = currentQuestion["choice" + number];
+        console.log(choice);
     });
     correctAnswer = true; 
 
 }; 
 
+ansButtons.forEach(choice => {
+    choice.addEventListener("click", e => {
+        //alert( "clicked me");
+        console.log(e.target);
+    if(!correctAnswer) return; 
+
+    correctAnswer = false;
+    const selectChoice = e.target;
+    //console.log(e.target); 
+    const selectAnswer = selectChoice.dataset["number"];
+    console.log(selectAnswer, currentQuestion.answer);
+
+    const setAns = "incorrect";
+    if (selectAnswer == currentQuestion.answer) {
+        setAns = "correct";
+    };
+    selectChoice.parentElement.classList.add(setAns);
+
+
+    newQuestion();
+    });
+    });
+
+startGame();
 
 
 
